d19e0fad5b79941043ab8c8c1529673c43992152
Raul Rita <raul.vinicius@live.com>
2023-04-03 20:29:53 -0300

    manipulando arrays

---
+++ semana-10-javascript/02-arrays/adicionando.js
@@ -0,0 +1,13 @@
+const notas = [10, 6.5, 8, 7.5];
+
+// nomeDoArray.push("param") - adiciona o parametro inserido na última posição do array
+
+notas.push(3)
+
+console.log(notas)
+
+// nomeDoArray.unshift("param") - adiciona o parametro inserido na primeira posição do array
+
+notas.unshift(1)
+
+console.log(notas)
